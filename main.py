f113692c3032011b1119d68d930a607d004ba6a6
Maciek Zemlo <38382353+yabool2001@users.noreply.github.com>
2023-10-28 14:53:42 +0200

    aaaa

--- main.py
+++ main.py
@@ -8,15 +8,13 @@ sys.path.append ( "/Users/mzeml/python/sim_nmea_uart/modules/" )
 import modules.my_serial as my_serial
 
 #nmea_source_filename = "source/2023.10.01.22.57_L86.txt"
-nmea_source_filename = "source/2023.10.28.03_L86.bin"
-#nmea_source_filename = "source/2023.10.28.04_L86.bin"
+#nmea_source_filename = "source/2023.10.28.03_L86.bin"
+nmea_source_filename = "source/2023.10.28.04_L86.bin"
 
 # Otwórz port szeregowy COM
 com = serial.Serial ()
 my_serial.set_serials_cfg ( com , 115200 , 'STLink' )
 my_serial.open_serial_ports ( com )
-
-#com = serial.Serial ( 'COM9' , 9600 , timeout = 1 )
 
 with open ( nmea_source_filename , 'r' ) as f :
     lines = f.readlines ()
@@ -24,13 +22,6 @@ with open ( nmea_source_filename , 'r' ) as f :
         com.write ( line.encode() )
         #com.write ( line )
         print ( line )
-        time.sleep ( 0.5 )
-
-
-#with open ( nmea_source_filename , 'rb' ) as f :
-#    b = f.read()
-#    for byte in b:
-#        com.write ( byte )
-#    print ( com.read () )
+        time.sleep ( 0.1 )
 
 my_serial.close_serial_ports ( com )
